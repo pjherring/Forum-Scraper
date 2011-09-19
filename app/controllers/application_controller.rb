ba57477a2b9b08f2672e9e2d17234fd19bc774d5
pherring@hugeinc.com <pherring@hugeinc.com>
2011-09-19 18:13:45 -0400

    added htaccess

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  USERS = { "camera_linear" => "__fl4sh__" }
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
+  
 end
