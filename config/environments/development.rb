1b799a09ddf8aed94b67680fcb00c19bc67ead76
pherring@hugeinc.com <pherring@hugeinc.com>
2011-09-20 17:32:45 -0400

    log level to info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Fp::Application.configure do
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
-  config.log_level = :debug
+  config.log_level = :info
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
